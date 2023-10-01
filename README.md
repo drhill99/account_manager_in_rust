1d1fa93f8aed9944c3816e98a2243443adbb15ff
Douglas Hill <drhill99@hotmail.com>
2023-09-30 20:37:22 -0600

    updated README file and added comments to main.rs

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# rust_module
-Repository contains a simple bank account program. Saves account data as JSON files.
+# Overview
+
+The primary purpose of this project was to learn Rust and explore the benefits that it provides. Rust introduces the concept of ownership and borrowing, which is enforced by the compiler and prevents race conditions. In addition to learning Rust, this project dives into the concepts of saving program data to memory.
+
+This software is an account management system, that allows the user to create accounts, holding a name of the account holder, an automatically generated account id, and an account balance. Accounts made during each session are saved to file as JSON files, and are loaded at each use. The program keeps track of what the next account id should be between sessions also, by saving the next account id in a JSON file and loading each session. The user can make deposits and withdrawals from accounts, look up an individual account or list off the account holder and id of all accounts. The user also has the ability to delete individual accounts by entering the name of the account holder and the account id when prompted.
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+The code for this project was written in VScode. The software was written in the Rust programming language. Building, compiling and running was done with cargo which is part of [rustup](https://www.rust-lang.org/tools/install). The command "cargo build" builds the project file and "cargo run" runs the program. The rustup link above will take you to the rust-lang.org website to down RUSTUP-INIT.EXE which makes cargo available to run this software.
+
+Rust is a language that emphasizes performance while enforcing memory safety. Due to the enforced memory safety, Rust is an excellent language for concurrent programming.
+
+# Useful Websites
+The following websites were extremely useful in building this project.
+
+- [rust-lang.org](https://www.rust-lang.org/)
+- [stackoverflow.com](https://stackoverflow.com/)
+- [Rust tutorialspoint.com](https://www.tutorialspoint.com/rust/index.htm)
+
+# Future Work
+
+
+- Additional account attributes such as transaction History
+- Additional methds to edit the account attributes    
+- Option to delete all Accounts
+- graphical elements
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::io::{self, Write};
-use std::fs::{self,File};
+use std::fs::{self,File, *};
 use std::io::prelude::*;
 use serde::{Serialize, Deserialize};
-
-
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct BasicAccountMembers {
@@ -112,24 +110,26 @@ fn get_user_string() -> String {
    
     trimmed_input
 }
-
+// creates account struct, taking signed 16 bit integer parameter
 fn create_account(account_id: i16) -> Account {
-    // let person: Person = create_person();
+    
     print!("Enter account holders name: ");
-    let _ = io::stdout().flush();
-    let name:&str = &get_user_string();
+    let _ = io::stdout().flush(); // flush newline character
+    let name:&str = &get_user_string(); // call for user defilned string variable
     print!("enter account balance: ");
-    let _ = io::stdout().flush();
-    let _balance: f32 = get_user_float();
+    let _ = io::stdout().flush(); // flush newline character
+    let _balance: f32 = get_user_float(); // call for user defined float variable
+    // create struct from above variables and function parameter
     let account = Account {
         id: account_id,
         account_holder:String::from(name),
         balance: _balance,
     };
+    // save the account struct as a json file in memory
     let _ = save_account_to_json(&account);
-    return account;    
+    return account; // return account struct
 }
-
+// creates Account struct in the same manner as the id tracker on line 144 below.
 fn load_account_from_json(file_name: String) -> io::Result<Account> {
     println!("loading account from json.......");
     let full_path = format!("AccountJsonFiles/{}", file_name);
@@ -142,42 +142,47 @@ fn load_account_from_json(file_name: String) -> io::Result<Account> {
 
     Ok(account)
 }
-
+// open the specified json file, creating a json string from the file contents, and create a struct
 fn load_account_id_tracker_struct_from_json() -> io::Result<BasicAccountMembers> {
-    // let mut current_account_id = Vec::new();
-    let full_path = String::from("account_id/current_id.json");
-    let mut file = File::open(full_path)?;
-    let mut json_string = String::new();
-    file.read_to_string(&mut json_string)?;
-    let current_id: BasicAccountMembers = serde_json::from_str(&json_string)?;
-    Ok(current_id)
+    
+    let full_path = String::from("account_id/current_id.json"); // file path string
+    let mut file = File::open(full_path)?; // create file from opening file at the given path
+    let mut json_string = String::new(); // create empty string
+    file.read_to_string(&mut json_string)?; // read the fill into the json string
+    let current_id: BasicAccountMembers = serde_json::from_str(&json_string)?; // create a struct from the json string
+    Ok(current_id) // return resulting struct to be checked and added to a vector
 }
-
+// call the above function and check for successfull creation of the struct
 fn load_account_id_tracker_to_vector() -> io::Result<Vec<BasicAccountMembers>> {
-    let mut current_account_id = Vec::new();
+    let mut current_account_id = Vec::new(); // empty vector to populate
+    // check the struct returned from the called function.
     match load_account_id_tracker_struct_from_json() {
         Ok(current_id_struct) => {
             println!("loaded existing json");
+            // if json exists, and creation of struct from json is successfull
+            // push struct to vector
             current_account_id.push(current_id_struct);
         }
         Err(_err) => {
+            // if no json exists, creation of the struct will fail, loading default struct into the vector 
             println!("No existing json, loaded default json");
             let basic_account_members: BasicAccountMembers = BasicAccountMembers { account_holder:String::from("current_id"),id: 0 };
             current_account_id.push(basic_account_members);
         }
     }
-    Ok(current_account_id)
+    Ok(current_account_id) // return resulting vector of account structs to be checked 
 }
 
-
+// generate a vector of strings, each being a json file found in the designated folderr
 fn generate_list_of_account_jsons() ->io::Result<Vec<String>> {
-    let mut json_files = Vec::new();
-
+    let mut json_files = Vec::new(); // vector to populate
+    // reads the files at the specified directory
     for entry in fs::read_dir("AccountJsonFiles")? {
-        let entry = entry?;
-        let path = entry.path();
-        if let Some(extension) = path.extension() {
-            if extension == "json" {
+        let entry = entry?; // load the successful result of reading the read_dir entry into the entry variable.
+        let path = entry.path(); // load the file path to the entry into the path variable
+        // check for files of a specific extension: json
+        if let Some(extension) = path.extension() { 
+            if extension == "json" { 
                 if let Some(file_name) = path.file_name(){
                     if let Some(file_str) = file_name.to_str() {
                         json_files.push(file_str.to_string());
@@ -187,51 +192,66 @@ fn generate_list_of_account_jsons() ->io::Result<Vec<String>> {
         }
     }
     
-    Ok(json_files)
+    Ok(json_files)// return resulting vector of strings
 }
-
+// gets a generated vector of json files in a given folder
+// and steps through the vector of files creating an account struct
+// for each json file in the vector
 fn load_all_json_files() -> io::Result<Vec<Account>> {
-    let mut accounts = Vec::new();
-    for file in generate_list_of_account_jsons()? {
-        println!("{}", file);
+    let mut accounts = Vec::new(); // empty vector to populate
+    // step through each json file in the vector
+    for file in generate_list_of_account_jsons()? { //generate the vector of files
+        println!("{}", file); // print name of loaded file
+        // check for correct loading of json into account with call to the 
+        // load_account_from_json function.
         match load_account_from_json(file) {
             Ok(loaded_account) => {
                 let account1 = loaded_account.clone();
-                accounts.push(account1);
+                accounts.push(account1); // populate accounts vector with generated structs
             }
             Err(err) => eprintln!("Error loading account from JSON: {}", err),
         }
     }
 
-    Ok(accounts)
+    Ok(accounts) // return resulting vector of Account structs
 }
 
-
+// take a reference to a mutable vector of accounts, and iterate through to find
+// and return a specific account struct based on the account id stuct member
 fn find_account(accounts: &mut Vec<Account>) -> Option<&mut Account> {
-    println!("Enter account id to search for: ");
-    let _ = io::stdout().flush();
-    let account_id = get_user_integer();
+    print!("Enter account id to search for: ");
+    let _ = io::stdout().flush(); // flushes newline character
+    let account_id = get_user_integer(); // takes user input and returns 16 bit signed integer
+    // steps through accounts in the vector until it finds the 
+    // unique account id entered by the user
     for account in accounts.iter_mut() {
         if account.id == account_id {
             return Some(account);
         }
     }
-    None
+    None // return None if no account found
 }
-
+// generate and return a 2 element tuple of a vector of "Account" structs, and a vector of "basis account members" structs
 fn program_startup() ->(Vec<Account>,Vec<BasicAccountMembers>) {
+    // check for json folders, create folders if missing. 
+    check_for_json_folders();
+    // create vector of account structs from existing json files, return type is a "Result".
+    // which is a special return type for I/O operations, which this is as the vector of structs
+    // is being created from json files in memory.
     let account_results = load_all_json_files();
-    
+    // check that the json files were correctly loaded into a vector of account structs
     let accounts = match account_results {
+        // return vector of accounts, not of result type if the conversion was successfull
         Ok(accounts) => {
             accounts
         }
         Err(err) => {
+            // return an empty vector if the conversion was unsuccessful
             eprintln!("Error loading accounts from JSON: {}", err);
             Vec::new()  // Return an empty vector or handle the error in another way
         }
     };  
-
+    // perform the same operations on a json file in a different project directory
     let account_id_results = load_account_id_tracker_to_vector();
 
     let current_id = match account_id_results {
@@ -325,7 +345,35 @@ fn menu(accounts: &mut Vec<Account>, current_id: &mut Vec<BasicAccountMembers>) 
     }
 
 }
+
+fn create_folder(path: &str) {
+    fs::create_dir(path).expect("Failed to create folder");
+}
+fn folder_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+fn check_for_json_folders() {
+    let folder1 = String::from("account_id");
+    let folder2 = String::from("AccountJsonFiles");
+    if !folder_exists(&folder1) {
+        create_folder(&folder1);
+        println!("Folder created: {}", folder1);
+    } else {
+        println!("Folder already exists: {}", folder1);
+    }
+
+    if !folder_exists(&folder2) {
+        create_folder(&folder2);
+        println!("Folder created: {}", folder2);
+    } else {
+        println!("Folder already exists: {}", folder2);
+    }
+
+}
 fn main() {
+    // 
+    
     let (mut accounts, mut current_id) = program_startup();
     let _ = menu(&mut accounts, &mut current_id);
 
